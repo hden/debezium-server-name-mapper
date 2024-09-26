eb7ed3bafbcf874723f6724f7b7ae8a6f4bac589
shotaogasawara <shota-ogasawara@bm-sms.co.jp>
2024-09-26 16:32:08 +0900

    一旦update

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-curl -s -X PUT 'http://localhost:8085/v1/projects/emulator/topics/foobar'
-curl -s -X PUT 'http://localhost:8085/v1/projects/emulator/subscriptions/baz' \
+curl -s -X PUT 'http://localhost:8681/v1/projects/emulator/topics/foobar'
+curl -s -X PUT 'http://localhost:8681/v1/projects/emulator/subscriptions/baz' \
 -H 'Content-Type: application/json' \
 -d '{
   "topic": "projects/emulator/topics/foobar"
