ecc6b03083dbdea3802d2cb8b4ae8412b6a3a073
shotaogasawara <shota-ogasawara@bm-sms.co.jp>
2024-09-27 13:45:56 +0900

    create receiveまでの時間差を計算できるようにする

--- pubsub-clients/subscribe.rb
+++ pubsub-clients/subscribe.rb
@@ -10,11 +10,11 @@ subscription = pubsub.subscription subscription_id
 subscriber   = subscription.listen do |received_message|
   received_at = Time.now # 他の処理で時間がかかると結果がブレるのでlistenしたら即時間を計測
   parsed_message = JSON.parse(received_message.data)
-  commited_at = parsed_message["payload"]["after"]["created_at"]
-  commited_at_parsed_utc = Time.parse(commited_at)
-  commited_at_parsed_jtc = commited_at_parsed_utc.getlocal("+09:00")
-
-  puts "Commited at: #{commited_at_parsed_jtc}, Published at: #{received_message.publish_time}, Received at: #{received_at}"
+  created_at = parsed_message["payload"]["after"]["created_at"]
+  created_at_parsed_utc = Time.parse(created_at)
+  created_at_parsed_jtc = created_at_parsed_utc.getlocal("+09:00")
+  create_receive_duration = received_at - created_at_parsed_jtc
+  puts "Commited at: #{created_at_parsed_jtc}, Published at: #{received_message.publish_time}, Received at: #{received_at}, CreateToReceiveDuration(ms): #{(create_receive_duration * 1000).to_i}"
   received_message.acknowledge!
 end
 
