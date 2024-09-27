f6a50cde2dad0792d991b41d27348f140815864b
shotaogasawara <shota-ogasawara@bm-sms.co.jp>
2024-09-27 13:35:43 +0900

    これでmessageのパフォーマンスは一応計測できる

--- pubsub-clients/subscribe.rb
+++ pubsub-clients/subscribe.rb
@@ -1,5 +1,5 @@
 require "google/cloud/pubsub"
-
+# require 'pry'
 subscription_id = "baz"
 
 pubsub = Google::Cloud::Pubsub.new(
@@ -8,7 +8,13 @@ pubsub = Google::Cloud::Pubsub.new(
 
 subscription = pubsub.subscription subscription_id
 subscriber   = subscription.listen do |received_message|
-  puts "Received message: #{received_message.data}"
+  received_at = Time.now # 他の処理で時間がかかると結果がブレるのでlistenしたら即時間を計測
+  parsed_message = JSON.parse(received_message.data)
+  commited_at = parsed_message["payload"]["after"]["created_at"]
+  commited_at_parsed_utc = Time.parse(commited_at)
+  commited_at_parsed_jtc = commited_at_parsed_utc.getlocal("+09:00")
+
+  puts "Commited at: #{commited_at_parsed_jtc}, Published at: #{received_message.publish_time}, Received at: #{received_at}"
   received_message.acknowledge!
 end
 
