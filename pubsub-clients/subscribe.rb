6135cc054918fdb0b97760f9d3b45a2fba9b9cc2
shotaogasawara <shota-ogasawara@bm-sms.co.jp>
2024-09-27 14:05:16 +0900

    update

--- pubsub-clients/subscribe.rb
+++ pubsub-clients/subscribe.rb
@@ -6,6 +6,8 @@ pubsub = Google::Cloud::Pubsub.new(
   project_id: "prj-dev-xuan-cloud-pubsub-poc",
 )
 
+puts "commited at, published at, received at, create to receive duration(ms)"
+
 subscription = pubsub.subscription subscription_id
 subscriber   = subscription.listen do |received_message|
   received_at = Time.now # 他の処理で時間がかかると結果がブレるのでlistenしたら即時間を計測
@@ -14,7 +16,7 @@ subscriber   = subscription.listen do |received_message|
   created_at_parsed_utc = Time.parse(created_at)
   created_at_parsed_jtc = created_at_parsed_utc.getlocal("+09:00")
   create_receive_duration = received_at - created_at_parsed_jtc
-  puts "Commited at: #{created_at_parsed_jtc}, Published at: #{received_message.publish_time}, Received at: #{received_at}, CreateToReceiveDuration(ms): #{(create_receive_duration * 1000).to_i}"
+  puts "#{created_at_parsed_jtc},#{received_message.publish_time},#{received_at},#{(create_receive_duration * 1000).to_i}"
   received_message.acknowledge!
 end
 
