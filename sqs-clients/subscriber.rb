5224b3b40a6d99e259b5c6e0d7744a9c94f50cec
shotaogasawara <shota-ogasawara@bm-sms.co.jp>
2024-10-01 15:25:25 +0900

    update

--- sqs-clients/subscriber.rb
+++ sqs-clients/subscriber.rb
@@ -2,57 +2,45 @@
 require "aws-sdk-sqs"
 require "aws-sdk-sts"
 
-def receive_messages(sqs_client, queue_url, max_number_of_messages = 10)
-
-  if max_number_of_messages > 10
-    puts "Maximum number of messages to receive must be 10 or less. " \
-      "Stopping program."
-    return
-  end
-
+def consume_messages(sqs_client, queue_url)
   response = sqs_client.receive_message(
     queue_url: queue_url,
-    max_number_of_messages: max_number_of_messages
+    max_number_of_messages: 1
   )
-
-  if response.messages.count.zero?
-    # puts "No messages to receive, or all messages have already " \
-    #   "been previously received."
-    return
-  end
+  return if response.messages.count.zero?
 
   response.messages.each do |message|
     puts "-" * 20
     puts "Message body: #{message.body}"
     puts "Message ID:   #{message.message_id}"
+    sqs_client.delete_message(
+      queue_url: queue_url,
+      receipt_handle: message.receipt_handle
+    )
   end
 
 rescue StandardError => e
   puts "Error receiving messages: #{e.message}"
 end
 
-def run_me
+def main
   region = "ap-northeast-1"
   queue_name = "ogasawara-sample-standard"
   max_number_of_messages = 10
 
   sts_client = Aws::STS::Client.new(region: region)
 
-  # For example:
-  # 'https://sqs.us-west-2.amazonaws.com/111111111111/my-queue'
   queue_url = "https://sqs." + region + ".amazonaws.com/" +
     sts_client.get_caller_identity.account + "/" + queue_name
 
   sqs_client = Aws::SQS::Client.new(region: region)
 
-  # puts "Receiving messages from queue '#{queue_name}'..."
-
-  receive_messages(sqs_client, queue_url, max_number_of_messages)
+  consume_messages(sqs_client, queue_url)
 end
 
 # 1秒ごとにrun_meを実行
 # どうやらSQSにはポーリングが必要らしい…
 loop do
-  run_me
+  main
   sleep 1
 end
