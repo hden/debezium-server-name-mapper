08c7f8826ab8052a4ed1c762eaa5dd228ec92fb8
shotaogasawara <shota-ogasawara@bm-sms.co.jp>
2024-10-03 13:56:29 +0900

    update

--- sqs-clients/subscriber.rb
+++ sqs-clients/subscriber.rb
@@ -1,6 +1,7 @@
 
 require "aws-sdk-sqs"
 require "aws-sdk-sts"
+# require 'pry'
 
 def consume_message(sqs_client, queue_url)
   response = sqs_client.receive_message(
@@ -10,9 +11,13 @@ def consume_message(sqs_client, queue_url)
   return if response.messages.count.zero?
 
   response.messages.each do |message|
-    puts "-" * 20
-    puts "Message body: #{message.body}"
-    puts "Message ID:   #{message.message_id}"
+    received_at = Time.now # 他の処理で時間がかかると結果がブレるのでlistenしたら即時間を計測
+    parsed_body = JSON.parse(message.body)
+    created_at = parsed_body["payload"]["after"]["created_at"]
+    created_at_parsed_utc = Time.parse(created_at)
+    created_at_parsed_jtc = created_at_parsed_utc.getlocal("+09:00")
+    create_receive_duration = received_at - created_at_parsed_jtc
+    puts "#{created_at_parsed_jtc},#{received_at},#{(create_receive_duration * 1000).to_i}"
     sqs_client.delete_message(
       queue_url: queue_url,
       receipt_handle: message.receipt_handle
@@ -35,6 +40,7 @@ def main
   consume_message(sqs_client, queue_url)
 end
 
+puts "commited at, received at, create to receive duration(ms)"
 # 1秒ごとにrun_meを実行
 # どうやらSQSにはポーリングが必要らしい…
 loop do
