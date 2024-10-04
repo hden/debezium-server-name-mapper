0b6ee57d01f7be215fba7bd818fea6944b099633
shotaogasawara <shota-ogasawara@bm-sms.co.jp>
2024-10-04 10:22:55 +0900

    処理をちゃんと書いたらパフォーマンス上がるわこれ

--- sqs-clients/subscriber.rb
+++ sqs-clients/subscriber.rb
@@ -1,12 +1,14 @@
 
 require "aws-sdk-sqs"
-require "aws-sdk-sts"
 
 def consume_message(sqs_client, queue_url)
-  response = sqs_client.receive_message(
-    queue_url: queue_url,
-    max_number_of_messages: 10 # なるべく一気にたくさんのmesasgeを取得させたい（最大10）
-  )
+  response = nil
+  measure_time do
+    response = sqs_client.receive_message(
+      queue_url: queue_url,
+      max_number_of_messages: 10 # なるべく一気にたくさんのmesasgeを取得させたい（最大10）
+    )
+  end
   return if response.messages.count.zero?
 
   received_at = Time.now # 他の処理で時間がかかると結果がブレるのでlistenしたら即時間を計測
@@ -17,10 +19,12 @@ def consume_message(sqs_client, queue_url)
     created_at_parsed_jtc = created_at_parsed_utc.getlocal("+09:00")
     create_receive_duration = received_at - created_at_parsed_jtc
     puts "#{created_at_parsed_jtc},#{received_at},#{(create_receive_duration * 1000).to_i}"
-    sqs_client.delete_message(
-      queue_url: queue_url,
-      receipt_handle: message.receipt_handle
-    )
+    measure_time do
+      sqs_client.delete_message(
+        queue_url: queue_url,
+        receipt_handle: message.receipt_handle
+      )
+    end
   end
 
 rescue StandardError => e
@@ -28,20 +32,29 @@ rescue StandardError => e
 end
 
 def main
-  region = "ap-northeast-1"
   queue_name = "ogasawara-sample.fifo"
-
-  sts_client = Aws::STS::Client.new(region: region)
-  queue_url = "https://sqs." + region + ".amazonaws.com/" +
-    sts_client.get_caller_identity.account + "/" + queue_name
-
+  region = "ap-northeast-1"
+  queue_url = "https://sqs.#{region}.amazonaws.com/356585129680/" + queue_name
   sqs_client = Aws::SQS::Client.new(region: region)
-  consume_message(sqs_client, queue_url)
+
+  puts "commited at, received at, create to receive duration(ms)"
+
+  # SQSはポーリングが必要なので短い時間角で処理を実行させる
+  loop do
+    consume_message(sqs_client, queue_url)
+    sleep 0.01 # 10ms
+  end
 end
 
-puts "commited at, received at, create to receive duration(ms)"
-# SQSはポーリングが必要なので短い時間角で処理を実行させる
-loop do
-  main
-  sleep 0.01 # 10ms
+def measure_time
+  start_time = Time.now
+  yield  # ブロック内の処理を実行
+  end_time = Time.now
+  elapsed_time = end_time - start_time
+
+  if ENV['DEBUG'] == 'true'
+    puts "Processing time: #{elapsed_time} seconds"
+  end
 end
+
+main
